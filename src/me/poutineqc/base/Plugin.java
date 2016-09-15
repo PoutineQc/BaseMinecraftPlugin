f057bd03bd790a423f270488f7b99da14540bf18
PoutineQc <chagnon.s21@gmail.com>
2016-09-15 12:04:41 -0400

    Add class PluginConfig

--- src/me/poutineqc/base/Plugin.java
+++ src/me/poutineqc/base/Plugin.java
@@ -1,5 +1,23 @@
 package me.poutineqc.base;
 
-public class Plugin {
+import org.bukkit.plugin.java.JavaPlugin;
 
+public class Plugin extends JavaPlugin {
+
+	private static Plugin plugin;
+	
+	@Override
+	public void onEnable() {
+		Plugin.plugin = this;
+	}
+
+	@Override
+	public void onDisable() {
+		
+	}
+	
+	public static Plugin get() {
+		return plugin;
+	}
+	
 }
---
+++ src/me/poutineqc/base/PluginConfig.java
@@ -0,0 +1,8 @@
+package me.poutineqc.base;
+
+public class PluginConfig {
+
+	public static final String fileName = "config";
+	
+	
+}
